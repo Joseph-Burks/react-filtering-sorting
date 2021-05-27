04ff87b81f68bf4549e8daea5facf0f9cbc4b728
Joseph-Burks <joe_burks_3@yahoo.com>
2021-05-27 12:12:39 -0500

    initial

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import MainContainer from './MainContainer'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MainContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/MainContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class MainContainer extends React.Component{
+
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
